0dcdc63e6a48c1aa20d6aee0d883f5ab6047bda0
Gabriela Contreras <gvcontreras21@gmail.com>
2021-04-19 20:03:02 -0300

    correcion con archivo de comentarios

--- src/MyLayout.js
+++ src/MyLayout.js
@@ -2,39 +2,28 @@ import React, { forwardRef } from 'react'
 import { Layout } from 'react-admin';
 import { AppBar, UserMenu, MenuItemLink, getResources } from 'react-admin';
 import SettingsIcon from '@material-ui/icons/Settings';
+import Logo from './img/logo.png';
+import { Component } from 'react';
 import { createElement } from 'react';
 import { useSelector } from 'react-redux';
 import { useMediaQuery } from '@material-ui/core';
 import { withRouter } from 'react-router-dom';
 import LabelIcon from '@material-ui/icons/Label';
 
+
 import { Sidebar } from 'react-admin';
 import { makeStyles } from '@material-ui/core/styles';
 
-const Menu = ({ onMenuClick }) => {
-  const open = useSelector(state => state.admin.ui.sidebarOpen);
-  const resources = useSelector(getResources);
-  return (
-		<div>
-			{resources.map(resource => (
-				<MenuItemLink
-					key={resource.name}
-					to={`/${resource.name}`}
-					primaryText={resource.options && resource.options.label || resource.name}
-					leftIcon={createElement(resource.icon)}
-					onClick={onMenuClick}
-					sidebarIsOpen={open}
-				/>
-			))}
-			<MenuItemLink
-				to="/custom-route"
-				primaryText="Miscellaneous"
-				leftIcon={<LabelIcon />}
-				onClick={onMenuClick}
-				sidebarIsOpen={open}
-			/>
-		</div>
-  );
+// const whiteLogo = require('./white.png')
+// src={'http://drive.google.com/uc?export=view&id=1z4_b0jdysIIYm1cFiigSMtPNXuMYqyRG'}
+
+const MyAppBar = props => {
+	const classes = useStyles();
+	return <AppBar className={classes.appBar} {...props} userMenu={<MyUserMenu />} >
+		<span className={classes.spacer} />
+		<img height='25px'  src={Logo} />
+		<span className={classes.spacer} />
+	</AppBar>
 }
 
 const ConfigurationMenu = forwardRef(({ onClick }, ref) => (
@@ -54,19 +43,37 @@ const MyUserMenu = props => (
 );
 
 const useSidebarStyles = makeStyles({
-  drawerPaper: {
-      backgroundColor: '#402217w',
-  },
+	drawerPaper: {
+		// backgroundColor: '#2B3E4E',
+	},
 });
 
+
 const MySidebar = props => {
-  const classes = useSidebarStyles();
-  return (
+	const classes = useSidebarStyles();
+	return (
 		<Sidebar classes={classes} {...props} />
-  );
+	);
 };
+const useStyles = makeStyles({
+	appBar: {
+		background: '#2B3E4E',
+		// backgroundImage: whiteLogo
+	},
+	spacer: {
+		flex: 1,
+	},
+});
 
 
-const MyAppBar = props => <AppBar {...props} userMenu={<MyUserMenu />} />;
+const useAppbarStyles = makeStyles({
+	colorPrimary: 'red'
+});
 
-export const MyLayout = props => <Layout {...props} appBar={MyAppBar} sidebar={MySidebar} />;
+
+
+export const MyLayout = props => {
+	const classes = useAppbarStyles();
+	return <Layout {...props} appBar={MyAppBar} sidebar={MySidebar} />
+	
+};
